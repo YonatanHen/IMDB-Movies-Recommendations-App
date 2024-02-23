ea78578403033227e3a03209b65abe44ff559437
yonatanh <yonatanh@jfrog.com>
2024-02-23 22:07:33 +0200

    updated the actor/director serach label

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.GUI.createTable import create_table
 
 table_data = None
 search_history = {}
+scraper = Scraper()
 
 def search_button_click():
     # Retrieve the values from the entry widgets
@@ -26,6 +27,7 @@ def search_button_click():
 
     create_table(root,table_data,winning_movie)
 
+def clear_button_click():
     title_var.set("")
     release_year_var.set("")
     genre_var.set("")
@@ -61,11 +63,11 @@ genre_entry = ttk.Combobox(
 ).grid(column=5, row=1)
 #genre_entry = ttk.Entry(frm,textvariable = genre_var, font=('calibre',10,'normal')).grid(column=5, row=1)
 
-actor_label = ttk.Label(frm, text = 'Actor:', font=('calibre',10, 'bold')).grid(column=6, row=1)
+actor_label = ttk.Label(frm, text = 'Actor/Director Name:', font=('calibre',10, 'bold')).grid(column=6, row=1)
 actor_entry = ttk.Entry(frm,textvariable = actors_var, font=('calibre',10,'normal')).grid(column=7, row=1)
 
-scraper = Scraper()
 search_btn = ttk.Button(frm, text="Search", command=search_button_click).grid(column=8, row=1)
-quit_btn = ttk.Button(frm, text="Quit", command=root.destroy).grid(column=9, row=1)
+clear_btn = ttk.Button(frm, text="Clear All", command=clear_button_click).grid(column=9, row=1)
+quit_btn = ttk.Button(frm, text="Quit", command=root.destroy).grid(column=10, row=1)
 
 root.mainloop()
